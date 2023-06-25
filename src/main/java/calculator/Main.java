b2c32e8973dfe54fbb553c2c80c5a6137794d930
Tatiana Voitekhovitch <stv.vag2020@gmail.com>
2023-06-25 16:12:49 +0200

    fixed mistake

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,7 +1,7 @@
 package calculator;
 
-/**
- * author Tatiana Voitekhovitch
+/*
+  author Tatiana Voitekhovitch
  */
 
 import java.util.Scanner;
@@ -9,219 +9,201 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите арифметическое выражение: : ");
+
+        System.out.print("Введите выражение: ");
         String input = scanner.nextLine();
 
         try {
             String result = calc(input);
-            System.out.println("Результат : " + result);
-        } catch (Exception e) {
-            System.out.println("Неверный ввод : " + e.getMessage());
+            System.out.println("Результат: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Ошибка: " + e.getMessage());
         }
-
-        scanner.close();
     }
 
     /**
-     * Выполняет арифметическое выражение между двумя операндами и возвращает результат в виде строки.
-     * Поддерживаемые операции: +, -, *, /
+     * Выполняет вычисление заданного арифметического выражения.
      *
      * @param input арифметическое выражение
      * @return результат вычисления
-     * @throws IllegalArgumentException при неверном формате ввода или неподдерживаемой операции
+     * @throws IllegalArgumentException если введено некорректное выражение
      */
+    public static String calc(String input) {
+        String[] elements = input.split(" ");
 
-    public static String calc(String input) throws Exception {
-// Разделяем ввод на операнды и оператор
-        String[] parts = input.split("\\ s + ");
-
-        if (parts.length != 3) {
-            throw new IllegalArgumentException("Неверный формат ввода. Формат: операнд оператор операнд !");
+        if (elements.length != 3) {
+            throw new IllegalArgumentException("Некорректное выражение. Ожидается два операнда и один оператор (+, -, *, /).");
         }
 
-        String operand1 = parts[0];
-        String operator = parts[1];
-        String operand2 = parts[2];
+        String operand1 = elements[0];
+        String operator = elements[1];
+        String operand2 = elements[2];
 
-        int num1;
-        int num2;
+        boolean isOperand1Roman = isRoman(operand1);
+        boolean isOperand2Roman = isRoman(operand2);
 
-        // Парсим операнды
-        try {
-            num1 = parseOperand(operand1);
-            num2 = parseOperand(operand2);
-        } catch (Exception e) {
-            throw new Exception("Invalid operands!");
-        }
+        if (isOperand1Roman && isOperand2Roman) {
+            // Оба операнда римские числа
+            int decimal1 = romanToDecimal(operand1);
+            int decimal2 = romanToDecimal(operand2);
 
-        // Выполняем арифметическую операцию в зависимости от оператора
-        int result;
-        switch (operator) {
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            case "/":
-                if (num2 == 0) {
-                    throw new IllegalArgumentException("Деление на ноль");
-                }
-                result = num1 / num2;
-                break;
-            default:
-                throw new IllegalArgumentException("Неподдерживаемый оператор. Поддерживаемые операторы: +, -, *, /");
-        }
+            int result = performCalculation(decimal1, operator, decimal2);
 
-        return formatResult(result, isRoman(operand1) && isRoman(operand2));
-    }
+            return formatResult(result);
+        } else if (!isOperand1Roman && !isOperand2Roman) {
+            // Оба операнда десятичные числа
+            int number1 = Integer.parseInt(operand1);
+            int number2 = Integer.parseInt(operand2);
 
-    /**
-     * Преобразует операнд в целое число.
-     *
-     * @param operand операнд
-     * @return преобразованное число
-     * @throws IllegalArgumentException при неверном формате операнда
-     */
-    private static int parseOperand(String operand) throws Exception {
-        if (isRoman(operand)) {
-            return romanToDecimal(operand);
-        } else if (isNumeric(operand)) {
-            int num = Integer.parseInt(operand);
-            if (num < 1 || num > 10) {
-                throw new IllegalArgumentException("Неверный операнд. ");
-            }
-            return num;
+            int result = performCalculation(number1, operator, number2);
+
+            return Integer.toString(result);
         } else {
-            throw new IllegalArgumentException("Неверный операнд!");
+            throw new IllegalArgumentException("Нельзя выполнять операции сразу с римскими и арабскими числами.");
         }
     }
 
     /**
-     * Проверяет, является ли переданная строка римским числом.
+     * Проверяет, является ли строка римским числом.
      *
-     * @param input проверяемая строка
-     * @return true, если строка является римским числом, иначе false
+     * @param input строка для проверки
+     * @return true, если строка римское число, иначе false
      */
-
     private static boolean isRoman(String input) {
-        // Римские числа состоят только из символов I, V, X, L, C, D, M
-        return input.matches("[IVXLCDM]+");
+        String romanNumerals = "IVXLCDM";
+
+        for (char symbol : input.toCharArray()) {
+            if (romanNumerals.indexOf(symbol) == -1) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
-     * Проверяет, является ли переданная строка числом.
-     *
-     * @param input проверяемая строка
-     * @return true, если строка является числом, иначе false
-     */
-    private static boolean isNumeric(String input) {
-        // Числа состоят только из цифр
-        return input.matches("\\d+");
-    }
-
-    /**
-     * Преобразует римское число в десятичное.
+     * Выполняет преобразование римского числа в десятичное.
      *
      * @param roman римское число
      * @return десятичное число
+     * @throws IllegalArgumentException если римское число некорректно
      */
     private static int romanToDecimal(String roman) {
         int decimal = 0;
         int prevValue = 0;
-// Проходимся по символам римского числа и вычисляем десятичное значение
+
+        // Проходимся по символам римского числа и вычисляем десятичное значение
         for (int i = roman.length() - 1; i >= 0; i--) {
-            for (int i = roman.length() - 1; i >= 0; i--) {
-                int currValue = getRomanValue(roman.charAt(i));
+            int value = getRomanValue(roman.charAt(i));
 
-                if (currValue < prevValue) {
-                    decimal -= currValue;
-                } else {
-                    decimal += currValue;
-                    prevValue = currValue;
-                }
-            }
-
-            return decimal;
-        }
-        /**
-         * Возвращает десятичное значение символа римского числа.
-         *
-         * @param symbol символ римского числа
-         * @return десятичное значение символа
-         * @throws IllegalArgumentException при некорректном символе
-         */
-        private static int getRomanValue ( char symbol){
-            switch (symbol) {
-                case 'I':
-                    return 1;
-                case 'V':
-                    return 5;
-                case 'X':
-                    return 10;
-                case 'L':
-                    return 50;
-                case 'C':
-                    return 100;
-                case 'D':
-                    return 500;
-                case 'M':
-                    return 1000;
-                default:
-                    throw new IllegalArgumentException("Некорректный символ римского числа: " + symbol);
-            }
-        }
-
-
-        /**
-         * Форматирует результат вычисления в соответствии с типом операндов.
-         * Если операнды римские числа, то результат будет возвращен в виде римского числа.
-         *
-         * @param result результат вычисления
-         * @return отформатированный результат
-         */
-
-        private static String formatResult ( int result, boolean isRoman){
-            if (isRoman) {
-                if (result <= 0) {
-                    throw new IllegalArgumentException("Результат меньше или равен нулю. Для римских чисел должны быть только положительные числа.");
-                }
-                return decimalToRoman(result);
+            if (value < prevValue) {
+                decimal -= value;
             } else {
-                return String.valueOf(result);
+                decimal += value;
+                prevValue = value;
             }
         }
-        /**
-         * Преобразует десятичное число в римское.
-         *
-         * @param number десятичное число
-         * @return римское число
-         */
-        public static String decimalToRoman ( int number){
-            if (number < 1 || number > 3999) {
-                throw new IllegalArgumentException("Number must be between 1 and 3999.");
-            }
-            // Массив значений римских чисел
-            int[] decimalValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-            // Массив символов римских чисел
-            String[] romanSymbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-            StringBuilder romanNumeral = new StringBuilder();
 
-// Проходимся по значениям римских чисел и добавляем нужное количество символов в римскую строку
-            for (int i = 0; i < decimalValues.length; i++) {
-                while (number >= decimalValues[i]) {
-                    romanNumeral.append(romanSymbols[i]);
-                    number -= decimalValues[i];
-                }
-            }
-
-            return romanNumeral.toString();
-        }
-
+        return decimal;
     }
+
+    /**
+     * Возвращает числовое значение символа римской цифры.
+     *
+     * @param symbol символ римской цифры
+     * @return числовое значение символа
+     * @throws IllegalArgumentException если символ римской цифры некорректен
+     */
+    private static int getRomanValue(char symbol) {
+        switch (symbol) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                throw new IllegalArgumentException("Некорректный символ римской цифры: " + symbol);
+        }
+    }
+
+    /**
+     * Выполняет арифметическую операцию между двумя числами.
+     *
+     * @param operand1 первый операнд
+     * @param operator арифметический оператор (+, -, *, /)
+     * @param operand2 второй операнд
+     * @return результат операции
+     * @throws IllegalArgumentException если оператор некорректен или деление на ноль
+     */
+    private static int performCalculation(int operand1, String operator, int operand2) {
+        switch (operator) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                if (operand2 == 0) {
+                    throw new IllegalArgumentException("Деление на ноль невозможно.");
+                }
+                return operand1 / operand2;
+            default:
+                throw new IllegalArgumentException("Некорректный оператор: " + operator);
+        }
+    }
+
+    /**
+     * Форматирует результат вычисления. Если результат отрицательный или ноль,
+     * возвращает исключение, так как в римской системе счисления отрицательные числа не представлены.
+     *
+     * @param result результат вычисления
+     * @return отформатированный результат
+     * @throws IllegalArgumentException если результат отрицательный или ноль
+     */
+    private static String formatResult(int result) {
+        if (result <= 0) {
+            throw new IllegalArgumentException("В римской системе счисления нет отрицательных чисел или ноля.");
+        }
+
+        return decimalToRoman(result);
+    }
+
+    /**
+     * Выполняет преобразование десятичного числа в римскую строку.
+     *
+     * @param decimal десятичное число
+     * @return римская строка
+     * @throws IllegalArgumentException если число не находится в диапазоне от 1 до 3999
+     */
+    private static String decimalToRoman(int decimal) {
+        if (decimal <= 0 || decimal >= 4000) {
+            throw new IllegalArgumentException("Число должно быть в диапазоне от 1 до 3999.");
+        }
+
+        StringBuilder roman = new StringBuilder();
+        int[] decimalValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] romanSymbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        for (int i = 0; i < decimalValues.length; i++) {
+            while (decimal >= decimalValues[i]) {
+                roman.append(romanSymbols[i]);
+                decimal -= decimalValues[i];
+            }
+        }
+
+        return roman.toString();
+    }
+}
+
 
